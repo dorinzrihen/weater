3e784203a44219a1859dced8be5c3a744702cf37
dorinzrihen <zrihend@gmail.com>
2020-09-08 15:44:08 +0300

    first commit

---
+++ weather-api.js
@@ -0,0 +1,109 @@
+
+const kelvin = 273.16;
+
+
+
+async function fetchWeatherApi(api) {
+    const res = await fetch(api);
+    const data = await res.json();
+    console.log(data)
+    return data;
+}
+async function getApiByCityName(cityName){
+    const data = await fetchWeatherApi(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=21cc8985dcb56222e056c7c649b11e0e`);
+
+    const weatherCard = {
+        name : data.name,
+        temp : data.main.temp,
+        weather : data.weather[0].main
+    }
+
+    setMiniCardInfo(weatherCard);
+}
+
+async function getApiByGeographicCoordinates(latitude,longitude){
+    const data = await fetchWeatherApi(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=21cc8985dcb56222e056c7c649b11e0e`);
+
+    const weatherCard = {
+        name : data.name,
+        temp : data.main.temp,
+        weather : data.weather[0].main,
+        sunrise : data.sys.sunrise,
+        sunset : data.sys.sunset,
+    }
+    setCardInfo(weatherCard);
+}
+
+//getApiByCityName('tel aviv');
+
+function getTime(unix){
+    let date = new Date(unix*1000);
+    return `${date.getHours()}:${date.getMinutes()}`
+}
+
+function setCardInfo(weatherCard){
+    const weatherContainer = document.querySelector('.card-container__info');
+    weatherContainer.appendChild(setInfo(weatherCard.name));
+    weatherContainer.appendChild(setInfo(`${Math.floor(weatherCard.temp - kelvin)}°C`));
+    weatherContainer.appendChild(setInfo(`Sunrise  ${getTime(weatherCard.sunrise)}`));
+    weatherContainer.appendChild(setInfo(`Sunset  ${getTime(weatherCard.sunset)}`));
+    weatherContainer.appendChild(setInfo(weatherCard.weather));
+}
+
+function setInfo(text){
+    const info = document.createElement('p');
+    info.textContent = text;
+    return info;
+}
+
+function setMiniCardInfo(weatherCard){
+    const weatherContainer = document.querySelector('.weather-container');
+    const miniWeatherCard = document.createElement('div');
+    
+    const infoCardSide = document.createElement('div');
+    const svgPick = document.createElement('div');
+    svgPick.classList.add('mini-card-img')
+    miniWeatherCard.classList.add('card-container');
+    infoCardSide.classList.add('mini-card');
+    svgPick.innerHTML = `<img src="/animated/cloudy-day-1.svg" alt="">`
+
+    infoCardSide.appendChild(setInfo(weatherCard.name));
+    infoCardSide.appendChild(setInfo(`${Math.floor(weatherCard.temp - kelvin)}°C`));
+    infoCardSide.appendChild(setInfo(weatherCard.weather));
+    miniWeatherCard.appendChild(infoCardSide);
+    miniWeatherCard.appendChild(svgPick);
+    weatherContainer.appendChild(miniWeatherCard);
+}
+
+
+//get current location
+function geoFindMe() {  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      getApiByGeographicCoordinates(latitude,longitude);
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  //current location function
+  geoFindMe();
+
+function getCityName(){
+    const inputCity = document.querySelector('#weather-city');
+    getApiByCityName(inputCity.value);
+}
+
+const btnGetCity = document.querySelector('.get-city');
+btnGetCity.addEventListener('click', getCityName)
